05120e953c7c06c90649b7cbc838387064560433
Kornel Kaczmarek <avcio2q@gmail.com>
2023-05-31 10:38:18 +0200

    added assigning/unassigning active class

---
+++ js/script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+function titleClickHandler(event){
+  event.preventDefault();
+  const clickedElement = this;
+
+  /* [DONE] remove class 'active' from all article links  */
+
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
+
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+
+  clickedElement.classList.add('active')
+
+  /* [DONE] remove class 'active' from all articles */
+
+  const activeArticles = document.querySelectorAll('.posts .active');
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
+
+  /* get 'href' attribute from the clicked link */
+
+  const attribute = clickedElement.getAttribute("href");
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  const article = document.querySelector(attribute)
+  
+  /* add class 'active' to the correct article */
+
+  article.classList.add('active')
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
